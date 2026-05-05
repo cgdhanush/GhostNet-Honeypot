2dd47034886e5e772454f482beb10c0a9e4c2f9b
dhanu <dhanushcg100@gmail.com>
2026-05-05 22:52:37 +0530

    updated

--- ghostnet/ghostnet.py
+++ ghostnet/ghostnet.py
@@ -7,9 +7,11 @@ logger = logging.getLogger(__name__)
 class GhostNet:
     def __init__(self):
         LoggingMixin.__init__(self, logger)
+    
+    def start_ssh_honeypot(self):
 
-    def start(self):
-        print("Starting GhostNet...")
+        logger.info("Starting SSH honeypot mode...")
+        from ghostnet.ssh.ssh import main
+        main()
 
-    def stop(self):
-        print("Stopping GhostNet...")
+
