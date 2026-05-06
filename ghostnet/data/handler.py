79327fbc7c3fe709ba15e4f0b9f074e0a67c71f2
dhanu <dhanushcg100@gmail.com>
2026-05-06 12:46:51 +0530

    corrected the handler

--- ghostnet/data/handler.py
+++ ghostnet/data/handler.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 import logging
 from pythonjsonlogger import jsonlogger
 
@@ -8,15 +8,19 @@ logger = logging.getLogger(__name__)
 class MongoHandler(logging.Handler):
     def __init__(self, collection):
         super().__init__()
-        self.collection = collection    
+        self.collection = collection
         self.setFormatter(jsonlogger.JsonFormatter())
 
     def emit(self, record):
         try:
             doc = record.__dict__.get("extra", {})
-            doc["message"] = record.getMessage()
             doc["level"] = record.levelname
+            doc["timestamp"] = datetime.fromtimestamp(record.created).isoformat()
+
+            message = record.getMessage()
+            for key, item in message:
+                doc[key] = item
+
             self.collection.insert_one(doc)
         except Exception:
             pass
-
